0c4ed2811c7c7fff1c0a09e7c78f881809e65c87
Isaac Flemming <elixic@gmail.com>
2015-09-14 20:36:52 -0700

    fixing config.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 require.config({
+  paths: {
+    "angular": "./lib/angular.min",
+    "angular-route": "./lib/angular-route.min"
+  },
   shim: {
     "lib/angular.min": {
       exports: "angular"
