9bef8b0ddcfce73e69cb37a47eab2b620bb6e023
Isaac Flemming <isaacflemming@Isaacs-MacBook-Pro.local>
2015-09-14 18:38:25 -0700

    typo.

--- js/app/app.js
+++ js/app/app.js
@@ -1,19 +1,17 @@
 define([
-    'angular',
-    'angular-route',
-    './Emailform/index'
+  'angular',
+  'angular-route',
+  './Emailform/index'
 ],
-    function (ng, ngRoute) {
-        'use strict';
-        var app = ng.module('pub', ['ngRoute', 'pub.EmailForm']);
+function (ng, ngRoute) {
+  'use strict';
+  var app = ng.module('pub', ['ngRoute', 'pub.EmailForm']);
 
-        app.config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/', {
-                    templateUrl: 'template/MailChimpTemplate',
-                    controller: 'EmailFormController'
-                });
-
-            }]);
-
-        return app;
+  app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'template/MailChimpTemplate.html',
+      controller: 'EmailFormController'
     });
+  }]);
+  return app;
+});
